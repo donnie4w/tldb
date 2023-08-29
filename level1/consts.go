0a35d8a194be34e60b51777e74ebb69afbc4376b
donnie <donnie4w@gmail.com>
2023-08-29 11:30:39 +0800

    Create consts.go

---
+++ level1/consts.go
@@ -0,0 +1,27 @@
+// Copyright (c) , donnie <donnie4w@gmail.com>
+// All rights reserved.
+//
+// github.com/donnie4w/tldb
+
+package level1
+
+const (
+	_Ping        = "1"
+	_Pong        = "2"
+	_Auth        = "3"
+	_Auth2       = "4"
+	_PonMerge    = "5"
+	_Pon         = "6"
+	_Pon2        = "7"
+	_Pon3        = "8"
+	_Time        = "9"
+	_SyncNode    = "a"
+	_SyncTx      = "b"
+	_SyncTxMerge = "c"
+	_CommitTx    = "d"
+	_CommitTx2   = "e"
+	_SubMq       = "f"
+	_PullData    = "g"
+	_ReInit      = "h"
+	_ProxyCall   = "i"
+)
