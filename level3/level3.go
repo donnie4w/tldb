beb5a1a867afa3e455502398c9bf0bd2ad60dbdd
donnie <donnie4w@gmail.com>
2023-08-29 11:31:41 +0800

    Create level3.go

---
+++ level3/level3.go
@@ -0,0 +1,22 @@
+// Copyright (c) , donnie <donnie4w@gmail.com>
+// All rights reserved.
+//
+// github.com/donnie4w/tldb
+
+package level3
+
+var Level3 = &_level3{}
+
+type _level3 struct{}
+
+func (this *_level3) PutMulti(ms map[string][]byte) (err error) {
+	return
+}
+
+func (this *_level3) Put(key, value []byte) (err error) {
+	return
+}
+
+func (this *_level3) Get(key []byte) (value []byte, err error) {
+	return
+}
