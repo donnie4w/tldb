ad6b9949ed09b25da2c676caba7c3e035040971a
donnie <donnie4w@gmail.com>
2023-09-23 11:19:20 +0800

    Update consts.go

--- level1/consts.go
+++ level1/consts.go
@@ -5,23 +5,24 @@
 
 package level1
 
-const (
-	_Ping        = "1"
-	_Pong        = "2"
-	_Auth        = "3"
-	_Auth2       = "4"
-	_PonMerge    = "5"
-	_Pon         = "6"
-	_Pon2        = "7"
-	_Pon3        = "8"
-	_Time        = "9"
-	_SyncNode    = "a"
-	_SyncTx      = "b"
-	_SyncTxMerge = "c"
-	_CommitTx    = "d"
-	_CommitTx2   = "e"
-	_SubMq       = "f"
-	_PullData    = "g"
-	_ReInit      = "h"
-	_ProxyCall   = "i"
+var (
+	_Ping        = []byte("1")
+	_Pong        = []byte("2")
+	_Auth        = []byte("3")
+	_Auth2       = []byte("4")
+	_PonMerge    = []byte("5")
+	_Pon         = []byte("6")
+	_Pon2        = []byte("7")
+	_Pon3        = []byte("8")
+	_Time        = []byte("9")
+	_SyncNode    = []byte("a")
+	_SyncTx      = []byte("b")
+	_SyncTxMerge = []byte("c")
+	_CommitTx    = []byte("d")
+	_CommitTx2   = []byte("e")
+	_PubMq       = []byte("f")
+	_PullData    = []byte("g")
+	_ReInit      = []byte("h")
+	_ProxyCall   = []byte("i")
+	_BroadToken  = []byte("j")
 )
