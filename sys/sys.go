6b8cf5b1d1d1911971d518fe710d3a92d94736c8
donnie <donnie4w@gmail.com>
2023-11-19 00:03:05 +0800

    Update sys.go

--- sys/sys.go
+++ sys/sys.go
@@ -2,29 +2,35 @@
 // All rights reserved.
 //
 // github.com/donnie4w/tldb
+//
+// Use of this source code is governed by a MIT-style license that can be
+// found in the LICENSE file
 
 package sys
 
 import (
 	"bytes"
 	"os"
-	"sync"
 	"sync/atomic"
 	"time"
 
+	"github.com/donnie4w/simplelog/logging"
 	. "github.com/donnie4w/tldb/container"
 	"github.com/donnie4w/tldb/stub"
 )
 
 // ///////////////////////////////////////////
-type STATTYPE int8   //
-var SYS_STAT = READY //
+type STATTYPE int8
+
+var SYS_STAT = READY
+
 const (
-	READY STATTYPE = 0 //就绪状态
-	PROXY STATTYPE = 1 //代理状态
-	RUN   STATTYPE = 2 //运行状态
-) //
-func IsREADY() bool { //
+	READY STATTYPE = 0
+	PROXY STATTYPE = 1
+	RUN   STATTYPE = 2
+)
+
+func IsREADY() bool {
 	return SYS_STAT == READY
 }
 func IsPROXY() bool {
@@ -38,7 +44,8 @@ func IsStandAlone() bool {
 	return CLUSTER_NUM == 0
 }
 
-var _stat_seq = int64(1) //
+var _stat_seq = int64(1)
+
 func SetStat(stat STATTYPE, timenano time.Duration) {
 	if timenano > 0 {
 		seq := atomic.AddInt64(&_stat_seq, 1)
@@ -60,51 +67,51 @@ func Start() {
 	if CMD {
 		Cmd()
 	} else {
-		var wg sync.WaitGroup
+		timlogo()
 		Service.BackForEach(func(_ int, s stub.Server) bool {
-			wg.Add(1)
 			go func() {
-				s.Serve(&wg)
+				defer func() { recover() }()
+				s.Serve()
 			}()
 			<-time.After(500 * time.Millisecond)
 			return true
 		})
-		wg.Wait()
+		select {}
 	}
 }
 
 func Stop() {
+	defer os.Exit(0)
 	Service.FrontForEach(func(_ int, s stub.Server) bool {
 		s.Close()
 		return true
 	})
-	os.Exit(0)
 }
 
-/*****************************************************/
-var IsClusRun func() bool
-var GetRemoteNode func() []*stub.RemoteNode
-var GetRunUUID func() []int64
-var SyncCount func() int64
-var TryRunToProxy func() (err error)
-var TryProxyToReady func() (err error)
-var ReSetStoreNodeNumber func(num int32) (err error)
-var LoadData2TLDB func(bs []byte, datetime string) (err error)
-var ForcedCoverageData2TLDB func(bs []byte, datetime string, limit int64) (err error)
-var Export func(tablename string) (_r bytes.Buffer, err error)
-var Level0Put func(key string, value []byte) (err error)
-var Level0Get func(key string) (value []byte, err error)
-var Client2Serve func(addr string) (err error)
-var BroadRmNode func() (err error)
-var ForceUnLock func(string)
-var Lock func(int64, string)
-var ReqToken func(int64, string) (err error)
-var TryLock func(string) (token string, ok bool)
-var UnLock func(string, string) (ok bool)
-var CcGet func() int64
-var CcPut func() int64
-var CountPut func() int64
-var CountGet func() int64
-var Cmd func()
-
-/*****************************************************/
+var (
+	IsClusRun               func() bool
+	GetRemoteNode           func() []*stub.RemoteNode
+	GetRunUUID              func() []int64
+	SyncCount               func() int64
+	TryRunToProxy           func() error
+	TryProxyToReady         func() error
+	ReSetStoreNodeNumber    func(int32) error
+	LoadData2TLDB           func([]byte, string) error
+	ForcedCoverageData2TLDB func([]byte, string, int64) error
+	Export                  func(string) (bytes.Buffer, error)
+	Level0Put               func(string, []byte) error
+	Level0Get               func(string) ([]byte, error)
+	Client2Serve            func(string) error
+	BroadRmNode             func() error
+	ForceUnLock             func(string)
+	Lock                    func(int64, string)
+	ReqToken                func(int64, string) error
+	TryLock                 func(string) (string, bool)
+	UnLock                  func(string, string) bool
+	CcGet                   func() int64
+	CcPut                   func() int64
+	CountPut                func() int64
+	CountGet                func() int64
+	Cmd                     func()
+	Log                     func() *logging.Logging
+)
