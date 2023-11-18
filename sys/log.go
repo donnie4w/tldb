53dd5d4fa887eb3af2c827c0feb4ce65a433e991
donnie <donnie4w@gmail.com>
2023-11-19 00:03:01 +0800

    Update log.go

--- sys/log.go
+++ sys/log.go
@@ -2,23 +2,49 @@
 // All rights reserved.
 //
 // github.com/donnie4w/tldb
+//
+// Use of this source code is governed by a MIT-style license that can be
+// found in the LICENSE file
 
 package sys
 
-import "fmt"
+import (
+	"fmt"
+)
 
-func SysLog(info string) (_r string) {
+func FmtLog(v ...any) {
+	info := fmt.Sprint(v...)
 	a, b := "", ""
 	ll := 80
 	if ll >= len(info) {
 		for i := 0; i < (ll-len(info))/2; i++ {
-			a = fmt.Sprint(a, "=")
+			a = a + "="
 		}
 		b = a
 		if ll > len(info)+len(a)*2 {
 			b = a + "="
 		}
 	}
-	_r = fmt.Sprint(a, info, b)
-	return
+	Log().Info(a, info, b)
+}
+
+func BlankLine() {
+	Log().Write([]byte("\n"))
+}
+
+func timlogo() {
+	_r := `
+              =============================================================================
+              ===        =============   ===           =======        =======           ===
+              ===             ===        ===           ===   ===      ===   ===         ===
+              ===             ===        ===           ===    ===     ===    ===        ===
+              ===             ===        ===           ===     ===    ===   ===         ===
+              ===             ===        ===           ===     ===    =======           ===
+              ===             ===        ===           ===     ===    ===   ===         ===
+              ===             ===        ===           ===    ===     ===    ===        ===
+              ===             ===        ===           ===   ===      ===   ===         ===
+              ===             ===        ===========   =======        =======           ===
+              =============================================================================
+	`
+	Log().Info(_r)
 }
