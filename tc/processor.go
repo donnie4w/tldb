559ea9353f07ba0ac9d1b2190a4e1bc2110c36ba
donnie <donnie4w@gmail.com>
2023-11-21 00:05:52 +0800

    Update processor.go

--- tc/processor.go
+++ tc/processor.go
@@ -2,7 +2,9 @@
 // All rights reserved.
 //
 // github.com/donnie4w/tldb
-
+//
+// Use of this source code is governed by a MIT-style license that can be
+// found in the LICENSE file
 package tc
 
 import (
