7df65da6837953abba66be98520fafe106765d9a
donnie <donnie4w@gmail.com>
2023-11-30 16:58:46 +0800

    Update pool.go

--- util/pool.go
+++ util/pool.go
@@ -1,10 +1,10 @@
-// Copyright (c) , donnie <donnie4w@gmail.com>
+// Copyright (c) 2023, donnie <donnie4w@gmail.com>
 // All rights reserved.
+// Use of this source code is governed by a BSD-style
+// license that can be found in the LICENSE file.
 //
 // github.com/donnie4w/tldb
 //
-// Use of this source code is governed by a MIT-style license that can be
-// found in the LICENSE file
 
 package util
 
