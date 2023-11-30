72c56cde43815bba62c31e1c5460d9d56ce78fbf
donnie <donnie4w@gmail.com>
2023-11-30 16:57:59 +0800

    Update tldb.go

--- tldb.go
+++ tldb.go
@@ -1,5 +1,7 @@
-// Copyright (c) , donnie <donnie4w@gmail.com>
+// Copyright (c) 2023, donnie <donnie4w@gmail.com>
 // All rights reserved.
+// Use of this source code is governed by a BSD-style
+// license that can be found in the LICENSE file.
 //
 // github.com/donnie4w/tldb
 package main
@@ -15,3 +17,26 @@ import (
 func main() {
 	Start()
 }
+
+
+//tldb database
+//https://github.com/donnie4w/tldb
+
+//database client
+//java:  https://github.com/donnie4w/tlcli-j  
+//go:    https://github.com/donnie4w/tlcli-go
+//python https://github.com/donnie4w/tlcli-py
+
+//orm 
+//java:  https://github.com/donnie4w/tlorm-java  
+//go:    https://github.com/donnie4w/tlorm-go
+
+//MQ client
+//java:  https://github.com/donnie4w/tlmq-j  
+//go:    https://github.com/donnie4w/tlmq-go
+//python https://github.com/donnie4w/tlmq-py
+//js     https://github.com/donnie4w/tlmq-js
+
+//website
+//https://tlnet.top
+//Email:donnie4w@gmail.com
